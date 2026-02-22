8feaf88e33894f4c812cfac5ae0121bd475a0d90
Paulo Tiago <paulotiagocm@hotmail.com>
2026-02-22 15:25:58 -0300

    deploy: v1.0.0 new config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
+  trailingSlash: true,
+  basePath: '/semana-treino',
 };
 
 export default nextConfig;
